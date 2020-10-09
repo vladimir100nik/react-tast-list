3418862fac54322248729767a0259381ba1f19a5
vladimir100nik <42648313+vladimir100nik@users.noreply.github.com>
2020-10-09 10:35:53 +0300

    Final

--- src/App.js
+++ src/App.js
@@ -1,26 +1,70 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useEffect} from 'react';
+import TodoList from "./Components/TodoList";
+import Context from "./context";
+import Loader from "./Loader";
+import Modal from "./Modal/Modal";
+
+const AddTodo = React.lazy(() => import('./Components/AddTodo'))
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [todos, setTodos] = React.useState([])
+    const [loading, setLoading] = React.useState(true)
+
+    useEffect(() => {
+        fetch('https://jsonplaceholder.typicode.com/todos?_limit=5')
+            .then(response => response.json())
+            .then(todos => {
+                setTimeout(() => {
+                    setTodos(todos)
+                    setLoading(false)
+                }, 2000)
+
+            })
+    }, [])
+
+
+    function toggleTodo(id) {
+        setTodos(todos.map(todo => {
+            if (todo.id === id) {
+                todo.completed = !todo.completed
+            }
+            return todo
+        }))
+    }
+
+    function removeTodo(id) {
+        setTodos(todos.filter(todo => todo.id !== id))
+    }
+
+    function addTodo(title) {
+        setTodos(todos.concat([{
+            title,
+            id: Date.now(),
+            completed: false
+        }]))
+    }
+
+    return (
+        <Context.Provider value={{removeTodo}}>
+            <div className="wrapper">
+                <h1>My list of tasks for the day</h1>
+                <Modal/>
+
+                <React.Suspense fallback={<p>loading...</p>}>
+                    <AddTodo onCreate={addTodo}/>
+                </React.Suspense>
+                {loading && <Loader/>}
+                {todos.length ? (<TodoList todos={todos} onToggle={toggleTodo}/>
+                ) : (
+                    loading ? null : <p>Your task list is empty</p>
+                )}
+
+            </div>
+        </Context.Provider>
+    )
+
+
 }
 
 export default App;
---
+++ src/Loader.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+
+ function Loader(){
+     return (
+    <div style={{display: 'flex', justifyContent: 'center', margin: '.5rem', marginTop:'20rem'}}>
+    <div className="lds-dual-ring"></div>
+</div>
+     )
+}
+
+
+export default Loader
